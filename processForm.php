2d1fa3d7e8f5f39245d7f647be343fae303544d9
[Sergi López] <socelsergi@gmail.com>
2024-05-16 13:36:44 +0200

    Process form php done

--- processForm.php
+++ processForm.php
@@ -1,5 +1,25 @@
 <?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $name = $_POST["name"];
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $message = $_POST["message"];
 
+
+    $to = "socelsergi@gmail.com";
+    $subject = "Nuevo mensaje del formulario de contacto";
+    $body = "Nombre: $name\n Usuario: $username\n Mail: $email\n Mensaje: $message";
+
+    if(mail($to, $subject, $body)){
+        echo "El mensaje se ha enviado";
+    }
+    else {
+        echo "Ha habido un error en el envío. Inténtalo más tarde";
+    }
+}
+else{
+    echo "Error: No se han recibido los datos del formulario.";
+}
 
 
 ?>
