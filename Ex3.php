1f1b4cc2c8e902bd8be5207c74bb28584a66bf62
[Sergi López] <socelsergi@gmail.com>
2024-05-27 13:54:50 +0200

    unwanted intro solve

--- Ex3.php
+++ Ex3.php
@@ -11,8 +11,8 @@ class MagicMethods{
         $this->occupation = $occupation;
     }
     public function __toString(){
-        return "Soy la función " .__FUNCTION__. " y el que la creó 
-        se llama $this->name. Tiene $this->age años y quiere trabajar como $this->occupation."; 
+        return "Soy la función " .__FUNCTION__. " y el que la creó, ". 
+        "se llama $this->name. Tiene $this->age años y quiere trabajar como $this->occupation."; 
     }
 }
 $instance = new MagicMethods("Sergi", 22, "programador Full Stack PHP");
