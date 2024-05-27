da583c9c2e2ff518e2b6abfcb55aee837253c9ec
[Sergi López] <socelsergi@gmail.com>
2024-05-27 13:53:13 +0200

    Creación clase construct y más atributos.

--- Ex3.php
+++ Ex3.php
@@ -1,11 +1,20 @@
 <?php
 
-
-
-function __toString($name){
-    return "Soy la función " .__FUNCTION__. " y el que la creó 
-    se llama $name"; 
+class MagicMethods{
+    private $name;
+    private $age;
+    private $occupation;
+   
+    public function __construct(string $name, int $age, string $occupation){
+        $this->name = $name;
+        $this->age = $age;
+        $this->occupation = $occupation;
+    }
+    public function __toString(){
+        return "Soy la función " .__FUNCTION__. " y el que la creó 
+        se llama $this->name. Tiene $this->age años y quiere trabajar como $this->occupation."; 
+    }
 }
-
-echo __toString("Sergi");
+$instance = new MagicMethods("Sergi", 22, "programador Full Stack PHP");
+echo $instance;
 ?>
